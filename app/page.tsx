506cb83fd7de23b96942deafa4407d426980d790
Umrzoq <umrzoq@superdispatch.com>
2024-01-15 20:22:49 +0500

    feat: updated params

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { getUserList } from "./api";
 import { UserList } from "./components/UserList";
 
 export default async function Home() {
-  const data = await getUserList({ page: 1, search: "" });
+  const data = await getUserList({ page: "1", search: "" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
